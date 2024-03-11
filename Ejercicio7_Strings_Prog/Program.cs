b9da8e028898e5696a39fa9f3dab4925c7bd07d4
DESKTOP-3ASMSNV\Ruben <rubenbernalramos@gmail.com>
2024-03-11 11:50:23 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio7_Strings_Prog/Program.cs
@@ -0,0 +1,45 @@
+﻿/***
+ * Ruben Bernal Ramos
+ * CSI1
+ */
+
+namespace Ejercicio7_Strings_Prog
+{
+    /// <summary>
+    /// Clase principal de la aplicación
+    /// rbr - 110324
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método principal de la aplicación
+        /// rbr - 110324
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            //Declaro las variables
+            string frase;
+            int numeroPalabras = 1;
+
+            //Pido la frase por consola
+            Console.WriteLine("Introduzca una frase: ");
+            frase = Console.ReadLine();
+
+            //Muestro la frase introducida
+            Console.WriteLine("La frase introducida es: " + frase);
+
+            //Calculo el número de palabras
+            for (int i=0; i<frase.Length; i++)
+            {
+                if (frase[i]==' ')
+                {
+                    numeroPalabras = numeroPalabras + 1;
+                }
+            }
+
+            //Muestro por consola el número de palabras
+            Console.WriteLine("La frase introducida tiene " + numeroPalabras + " palabras.");
+        }
+    }
+}
